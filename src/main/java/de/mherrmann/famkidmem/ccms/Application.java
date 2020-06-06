6be705344d13a04c6bae0b9c07e2d71a011245f5
tomatenmark <mh88@mailbox.org>
2020-06-06 15:58:58 +0200

    required to mock settings

--- src/main/java/de/mherrmann/famkidmem/ccms/Application.java
+++ src/main/java/de/mherrmann/famkidmem/ccms/Application.java
@@ -4,6 +4,8 @@ import de.mherrmann.famkidmem.ccms.settings.Settings;
 import de.mherrmann.famkidmem.ccms.settings.SettingsReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -11,11 +13,24 @@ public class Application {
     private static Settings settings;
 
     public static void main(String[] args) {
-        settings = SettingsReader.readSettings();
+        setSettings(SettingsReader.readSettings());
         SpringApplication.run(Application.class, args);
+    }
+
+    public static void setSettings(Settings settings){
+        Application.settings = settings;
     }
 
     public static Settings getSettings(){
         return settings;
     }
+
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
+
+
