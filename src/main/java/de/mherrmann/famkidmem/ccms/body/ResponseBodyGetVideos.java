9c8ff5a896c057518ace838fd4f25eab18b208d5
tomatenmark <mh88@mailbox.org>
2020-06-07 20:17:40 +0200

    we only need the videos, not the filters

---
+++ src/main/java/de/mherrmann/famkidmem/ccms/body/ResponseBodyGetVideos.java
@@ -0,0 +1,30 @@
+package de.mherrmann.famkidmem.ccms.body;
+
+import de.mherrmann.famkidmem.ccms.item.User;
+import de.mherrmann.famkidmem.ccms.item.Video;
+
+import java.util.List;
+
+public class ResponseBodyGetVideos extends ResponseBody {
+
+    private List<Video> videos;
+
+    public ResponseBodyGetVideos(){}
+
+    public ResponseBodyGetVideos(List<Video> videos){
+        super("ok", "Successfully get videos");
+        this.setVideos(videos);
+    }
+
+    public ResponseBodyGetVideos(Exception ex){
+        super("error", ex.getMessage(), ex);
+    }
+
+    public List<Video> getVideos() {
+        return videos;
+    }
+
+    public void setVideos(List<Video> videos) {
+        this.videos = videos;
+    }
+}
