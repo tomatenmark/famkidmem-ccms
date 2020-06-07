22542c1605e1683fabe76bfe2b9a137d22dfeef9
tomatenmark <mh88@mailbox.org>
2020-06-07 20:34:38 +0200

    added skeletons for view loading

--- src/main/java/de/mherrmann/famkidmem/ccms/controller/VideoController.java
+++ src/main/java/de/mherrmann/famkidmem/ccms/controller/VideoController.java
@@ -3,6 +3,8 @@ package de.mherrmann.famkidmem.ccms.controller;
 import de.mherrmann.famkidmem.ccms.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VideoController {
@@ -12,5 +14,35 @@ public class VideoController {
     @Autowired
     public VideoController(VideoService videoService) {
         this.videoService = videoService;
+    }
+
+    @GetMapping(value = "/video/index")
+    public String loadIndexView(Model model){
+        videoService.fillIndexModel(model);
+        return "video/index";
+    }
+
+    @GetMapping(value = "/video/add")
+    public String loadAddVideoView(Model model){
+        model.addAttribute("post", false);
+        return "video/index";
+    }
+
+    @GetMapping(value = "/video/add")
+    public String loadEditDataView(Model model){
+        videoService.fillEditDataModel(model);
+        return "video/index";
+    }
+
+    @GetMapping(value = "/video/add")
+    public String loadReplaceThumbnailView(Model model){
+        videoService.fillReplaceThumbnailModel(model);
+        return "video/index";
+    }
+
+    @GetMapping(value = "/video/add")
+    public String loadRemoveVideoView(Model model){
+        videoService.fillRemoveVideoModel(model);
+        return "video/index";
     }
 }
--- src/main/java/de/mherrmann/famkidmem/ccms/service/VideoService.java
+++ src/main/java/de/mherrmann/famkidmem/ccms/service/VideoService.java
@@ -1,8 +1,78 @@
 package de.mherrmann.famkidmem.ccms.service;
 
+import de.mherrmann.famkidmem.ccms.body.ResponseBodyGetVideos;
+import de.mherrmann.famkidmem.ccms.exception.WebBackendException;
+import de.mherrmann.famkidmem.ccms.item.Video;
+import de.mherrmann.famkidmem.ccms.utils.ExceptionUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class VideoService {
 
+    private final ConnectionService connectionService;
+    private final ExceptionUtil exceptionUtil;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VideoService.class);
+
+    @Autowired
+    public VideoService(ConnectionService connectionService, ExceptionUtil exceptionUtil) {
+        this.connectionService = connectionService;
+        this.exceptionUtil = exceptionUtil;
+    }
+
+    public void fillIndexModel(Model model){
+        try {
+            model.addAttribute("users", getVideos());
+            model.addAttribute("success", true);
+        } catch(Exception ex){
+            exceptionUtil.handleException(ex, model, LOGGER);
+            model.addAttribute("users", Collections.emptyList());
+        }
+    }
+
+    public void fillEditDataModel(Model model){
+        /* TODO: fill:
+            * video
+            * post=false
+            * comma-separated stringified list of years
+            * comma-separated stringified list of persons
+            * year, read from timestamp
+            * month, read from timestamp (0, if video.showDataValues < 2 )
+            * day, read from timestamp   (0, if video.showDataValues < 3 )
+         */
+    }
+
+    public void fillReplaceThumbnailModel(Model model){
+        /* TODO: fill:
+         * post=false
+         * title (video.title)
+         */
+    }
+
+    public void fillRemoveVideoModel(Model model){
+        /* TODO: fill:
+         * post=false
+         * title (video.title)
+         * m3u8 (base64 content of m3u8 file from server)
+         * m3u8 filename
+         * thumbnail filename
+         */
+    }
+
+    @SuppressWarnings("unchecked") //we know, the assignment will work
+    private List<Video> getVideos() throws Exception {
+        ResponseEntity<ResponseBodyGetVideos> response = connectionService.doGetRequest( "/ccms/edit/video/get/");
+        if(response.getStatusCode().is2xxSuccessful()){
+            return response.getBody().getVideos();
+        }
+        throw new WebBackendException(response.getBody());
+    }
 }
