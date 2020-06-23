29b0a70b52d2027dcd22b36d01278ec8e78c3f05
tomatenmark <mh88@mailbox.org>
2020-06-23 19:10:21 +0200

    we will do it in frontend

--- src/main/java/de/mherrmann/famkidmem/ccms/service/video/VideoIndexService.java
+++ src/main/java/de/mherrmann/famkidmem/ccms/service/video/VideoIndexService.java
@@ -1,9 +1,11 @@
 package de.mherrmann.famkidmem.ccms.service.video;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.mherrmann.famkidmem.ccms.body.ResponseBodyGetVideos;
 import de.mherrmann.famkidmem.ccms.exception.WebBackendException;
 import de.mherrmann.famkidmem.ccms.item.Video;
 import de.mherrmann.famkidmem.ccms.service.ConnectionService;
+import de.mherrmann.famkidmem.ccms.utils.CryptoUtil;
 import de.mherrmann.famkidmem.ccms.utils.ExceptionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,29 +14,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
-import java.util.TimeZone;
 
 @Service
 public class VideoIndexService {
 
     private final ConnectionService connectionService;
     private final ExceptionUtil exceptionUtil;
+    private final CryptoUtil cryptoUtil;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(VideoIndexService.class);
 
     @Autowired
-    public VideoIndexService(ConnectionService connectionService, ExceptionUtil exceptionUtil) {
+    public VideoIndexService(ConnectionService connectionService, ExceptionUtil exceptionUtil, CryptoUtil cryptoUtil) {
         this.connectionService = connectionService;
         this.exceptionUtil = exceptionUtil;
+        this.cryptoUtil = cryptoUtil;
     }
 
     public void fillIndexModel(Model model){
         try {
-            model.addAttribute("videos", getVideos());
+            List<Video> videos = getVideos();
+            model.addAttribute("videosJson", asJsonString(videos));
+            model.addAttribute("videosCount", videos.size());
             model.addAttribute("success", true);
         } catch(Exception ex){
             exceptionUtil.handleException(ex, model, LOGGER);
@@ -42,33 +45,21 @@ public class VideoIndexService {
         }
     }
 
-    public String getFormattedDate(Video video){
-        LocalDateTime date =
-                LocalDateTime.ofInstant(Instant.ofEpochMilli(video.getTimestamp().getTime()),
-                        TimeZone.getDefault().toZoneId());
-        String dateFormatted = String.valueOf(date.getYear());
-        if(video.getShowDateValues() >= 6){
-            dateFormatted += "-" + String.valueOf(date.getMonth());
-        }
-        if(video.getShowDateValues() == 7){
-            dateFormatted += "-" + String.valueOf(date.getDayOfMonth());
-        }
-        return dateFormatted;
-    }
-
-    public String getShortDescription(String description){
-        description = description.substring(0,10);
-        description = description.replaceAll("\\s+[^\\s]*$", "");
-        description = description.trim();
-        return description + "...";
-    }
-
     @SuppressWarnings("unchecked") //we know, the assignment will work
     private List<Video> getVideos() throws Exception {
         ResponseEntity<ResponseBodyGetVideos> response = connectionService.doGetVideosRequest();
-        if(response.getStatusCode().is2xxSuccessful()){
-            return response.getBody().getVideos();
+        if(!response.getStatusCode().is2xxSuccessful()){
+            throw new WebBackendException(response.getBody());
         }
-        throw new WebBackendException(response.getBody());
+        return response.getBody().getVideos();
     }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
