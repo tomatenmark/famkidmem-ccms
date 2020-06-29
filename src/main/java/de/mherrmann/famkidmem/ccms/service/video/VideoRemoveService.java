d382be5455c83d4faabfc9ad74fbdbeb4fe3de23
tomatenmark <mh88@mailbox.org>
2020-06-29 19:44:43 +0200

    loadView (incl. tests)

---
+++ src/main/java/de/mherrmann/famkidmem/ccms/service/video/VideoRemoveService.java
@@ -0,0 +1,75 @@
+package de.mherrmann.famkidmem.ccms.service.video;
+
+import de.mherrmann.famkidmem.ccms.body.ResponseBodyContentFileBase64;
+import de.mherrmann.famkidmem.ccms.body.ResponseBodyGetVideos;
+import de.mherrmann.famkidmem.ccms.exception.WebBackendException;
+import de.mherrmann.famkidmem.ccms.item.Video;
+import de.mherrmann.famkidmem.ccms.service.ConnectionService;
+import de.mherrmann.famkidmem.ccms.utils.ExceptionUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+@Service
+public class VideoRemoveService {
+
+    private final ConnectionService connectionService;
+    private final ExceptionUtil exceptionUtil;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VideoRemoveService.class);
+
+
+    @Autowired
+    public VideoRemoveService(ConnectionService connectionService, ExceptionUtil exceptionUtil) {
+        this.connectionService = connectionService;
+        this.exceptionUtil = exceptionUtil;
+    }
+
+    public void fillRemoveVideoModel(Model model, String designator){
+        model.addAttribute("post", false);
+        model.addAttribute("title", designator);
+        try {
+            Video video = getVideo(designator);
+            model.addAttribute("thumbnailFilename", video.getThumbnail().getFilename());
+            model.addAttribute("m3u8Filename", video.getM3u8().getFilename());
+            model.addAttribute("m3u8", getM3u8(video.getM3u8().getFilename()));
+            model.addAttribute("success", true);
+        } catch(Exception ex){
+            LOGGER.error("Error. Could not replace thumbnail", ex);
+            exceptionUtil.handleException(ex, model, LOGGER);
+        }
+    }
+
+    public void deleteVideo(Model model, String title){
+        //TODO: delete video (get names of files to delete from request)
+    }
+
+    @SuppressWarnings("unchecked") //we know, the assignment will work
+    private Video getVideo(String designator) throws Exception {
+        designator = designator.replace('_', '/').replace('-', '+');
+        ResponseEntity<ResponseBodyGetVideos> response = connectionService.doGetSingleVideoRequest(designator);
+        if(!response.getStatusCode().is2xxSuccessful()){
+            throw new WebBackendException(response.getBody());
+        }
+        return response.getBody().getVideos().get(0);
+    }
+
+    @SuppressWarnings("unchecked") //we know, the assignment will work
+    private String getM3u8(String filename) throws Exception {
+        try {
+            ResponseEntity<ResponseBodyContentFileBase64> response = connectionService.doGetBase64(filename);
+            if(!response.getStatusCode().is2xxSuccessful()){
+                throw new WebBackendException(response.getBody());
+            }
+            LOGGER.info("successfully get base64 of file {}", filename);
+            return response.getBody().getBase64();
+        } catch (Exception ex){
+            LOGGER.error("could not get base64 of file {}", filename, ex);
+            throw ex;
+        }
+    }
+
+}
--- src/test/java/de/mherrmann/famkidmem/ccms/management/video/VideoManagementTestRemove.java
+++ src/test/java/de/mherrmann/famkidmem/ccms/management/video/VideoManagementTestRemove.java
@@ -1,6 +1,11 @@
 package de.mherrmann.famkidmem.ccms.management.video;
 
+import de.mherrmann.famkidmem.ccms.Application;
 import de.mherrmann.famkidmem.ccms.TestUtil;
+import de.mherrmann.famkidmem.ccms.body.ResponseBodyContentFileBase64;
+import de.mherrmann.famkidmem.ccms.body.ResponseBodyGetVideos;
+import de.mherrmann.famkidmem.ccms.item.Video;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,14 +13,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
 public class VideoManagementTestRemove {
+
+    private static final String M3U8_BASE64 = "bTN1OA==";
 
     @MockBean
     private RestTemplate restTemplate;
@@ -33,7 +49,21 @@ public class VideoManagementTestRemove {
 
     @Test
     public void shouldLoadRemoveVideoView() throws Exception {
-        //TODO: implement test (see VideoManagementTestIndex.shouldLoadIndexView)
+        List<Video> videos = testUtil.createVideosList();
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/edit/video/get/video1", HttpMethod.GET, testUtil.createTestHttpEntityNoBody(), ResponseBodyGetVideos.class))
+                .willReturn(testUtil.createTestResponseEntityGetVideos(videos));
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/edit/video/base64/m3u8", HttpMethod.GET, testUtil.createTestHttpEntityNoBody(), ResponseBodyContentFileBase64.class))
+                .willReturn(testUtil.createTestResponseEntityContentFileBase64(M3U8_BASE64));
+
+
+        this.mockMvc.perform(get("/video/remove/video1"))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(model().attribute("title", Matchers.equalTo("video1")))
+                .andExpect(model().attribute("thumbnailFilename", Matchers.equalTo("thumbnail")))
+                .andExpect(model().attribute("m3u8Filename", Matchers.equalTo("m3u8")))
+                .andExpect(model().attribute("m3u8", Matchers.equalTo(M3U8_BASE64)))
+                .andExpect(model().attribute("post", Matchers.equalTo(false)))
+                .andExpect(model().attribute("success", Matchers.equalTo(true)));
     }
 
     //TODO: add tests: shouldRemoveVideoVideo, shouldFailRemoveVideoCausedByBadRequestResponse, shouldFailRemoveVideoCausedByConnectionFailure
