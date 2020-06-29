f9eaaee4a51c90d5429a7f67f7c8715fee900fae
tomatenmark <mh88@mailbox.org>
2020-06-29 21:40:03 +0200

    implementation of deletion incl. tests

--- src/main/java/de/mherrmann/famkidmem/ccms/service/video/VideoRemoveService.java
+++ src/main/java/de/mherrmann/famkidmem/ccms/service/video/VideoRemoveService.java
@@ -1,6 +1,7 @@
 package de.mherrmann.famkidmem.ccms.service.video;
 
 import de.mherrmann.famkidmem.ccms.Application;
+import de.mherrmann.famkidmem.ccms.body.ResponseBody;
 import de.mherrmann.famkidmem.ccms.body.ResponseBodyContentFileBase64;
 import de.mherrmann.famkidmem.ccms.body.ResponseBodyGetVideos;
 import de.mherrmann.famkidmem.ccms.exception.WebBackendException;
@@ -10,9 +11,12 @@ import de.mherrmann.famkidmem.ccms.utils.ExceptionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class VideoRemoveService {
@@ -44,13 +48,22 @@ public class VideoRemoveService {
             model.addAttribute("masterKey", Application.getSettings().getMasterKey());
             model.addAttribute("success", true);
         } catch(Exception ex){
-            LOGGER.error("Error. Could not replace thumbnail", ex);
+            LOGGER.error("Error. Could not load view", ex);
             exceptionUtil.handleException(ex, model, LOGGER);
         }
     }
 
-    public void deleteVideo(Model model, String title){
-        //TODO: delete video (get names of files to delete from request)
+    public void deleteVideo(Model model, HttpServletRequest request, String designator){
+        model.addAttribute("post", true);
+        model.addAttribute("title", designator);
+        try {
+            deleteVideoFiles(request);
+            removeVideo(designator);
+            model.addAttribute("success", true);
+        } catch(Exception ex){
+            LOGGER.error("Error. Could not remove video", ex);
+            exceptionUtil.handleException(ex, model, LOGGER);
+        }
     }
 
     @SuppressWarnings("unchecked") //we know, the assignment will work
@@ -74,6 +87,41 @@ public class VideoRemoveService {
             return response.getBody().getBase64();
         } catch (Exception ex){
             LOGGER.error("could not get base64 of file {}", filename, ex);
+            throw ex;
+        }
+    }
+
+    private void deleteVideoFiles(HttpServletRequest request) throws Exception {
+        deleteVideoFile(request.getParameter("thumbnailFilename"));
+        deleteVideoFile(request.getParameter("m3u8Filename"));
+        for(int i = 0; i < Integer.valueOf(request.getParameter("tsFilesCount")); i++){
+            deleteVideoFile(String.format(request.getParameter("tsFilesName"), i));
+        }
+    }
+
+    @SuppressWarnings("unchecked") //we know, the assignment will work
+    private void deleteVideoFile(String filename) throws Exception {
+        try {
+            ResponseEntity<String> response = connectionService.doDeleteFileRequest(filename);
+            if(!response.getStatusCode().is2xxSuccessful()){
+                throw new WebBackendException();
+            }
+        } catch (Exception ex){
+            LOGGER.error("could not delete file {}. deletion skipped", filename, ex);
+            throw ex;
+        }
+    }
+
+    @SuppressWarnings("unchecked") //we know, the assignment will work
+    private void removeVideo(String designator) throws Exception {
+        designator = designator.replace('_', '/').replace('-', '+');
+        try {
+            ResponseEntity<ResponseBody> response = connectionService.doDeleteVideoRequest(designator);
+            if(!response.getStatusCode().is2xxSuccessful()){
+                throw new WebBackendException(response.getBody());
+            }
+        } catch (Exception ex){
+            LOGGER.error("could not delete video {} (encrypted title)", designator, ex);
             throw ex;
         }
     }
--- src/test/java/de/mherrmann/famkidmem/ccms/management/video/VideoManagementTestRemove.java
+++ src/test/java/de/mherrmann/famkidmem/ccms/management/video/VideoManagementTestRemove.java
@@ -2,6 +2,7 @@ package de.mherrmann.famkidmem.ccms.management.video;
 
 import de.mherrmann.famkidmem.ccms.Application;
 import de.mherrmann.famkidmem.ccms.TestUtil;
+import de.mherrmann.famkidmem.ccms.body.ResponseBody;
 import de.mherrmann.famkidmem.ccms.body.ResponseBodyContentFileBase64;
 import de.mherrmann.famkidmem.ccms.body.ResponseBodyGetVideos;
 import de.mherrmann.famkidmem.ccms.item.Video;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
@@ -23,6 +25,7 @@ import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -71,5 +74,66 @@ public class VideoManagementTestRemove {
                 .andExpect(model().attribute("success", Matchers.equalTo(true)));
     }
 
-    //TODO: add tests: shouldRemoveVideoVideo, shouldFailRemoveVideoCausedByBadRequestResponse, shouldFailRemoveVideoCausedByConnectionFailure
+    @Test
+    public void shouldRemoveVideo() throws Exception {
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/delete/thumbnail", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), String.class))
+                .willReturn(ResponseEntity.ok("ok"));
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/delete/m3u8", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), String.class))
+                .willReturn(ResponseEntity.ok("ok"));
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/delete/sequence.0.ts", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), String.class))
+                .willReturn(ResponseEntity.ok("ok"));
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/delete/sequence.1.ts", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), String.class))
+                .willReturn(ResponseEntity.ok("ok"));
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/edit/video/delete/video1", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), ResponseBody.class))
+                .willReturn(testUtil.createTestResponseEntityStatusOk());
+
+        this.mockMvc.perform(post("/video/remove/video1")
+                .param("thumbnailFilename", "thumbnail")
+                .param("m3u8Filename", "m3u8")
+                .param("tsFilesName", "sequence.%d.ts")
+                .param("tsFilesCount", "2"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("success", Matchers.equalTo(true)))
+                .andExpect(model().attribute("post", Matchers.equalTo(true)));
+    }
+
+    @Test
+    public void checkCallRemoveVideo() throws Exception {
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/delete/thumbnail", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), String.class))
+                .willReturn(ResponseEntity.ok("ok"));
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/delete/m3u8", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), String.class))
+                .willReturn(ResponseEntity.ok("ok"));
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/delete/sequence.0.ts", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), String.class))
+                .willReturn(ResponseEntity.ok("ok"));
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/delete/sequence.1.ts", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), String.class))
+                .willReturn(ResponseEntity.ok("ok"));
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/edit/video/delete/video1", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), ResponseBody.class))
+                .willThrow(new RuntimeException("was called"));
+
+        this.mockMvc.perform(post("/video/remove/video1")
+                .param("thumbnailFilename", "thumbnail")
+                .param("m3u8Filename", "m3u8")
+                .param("tsFilesName", "sequence.%d.ts")
+                .param("tsFilesCount", "2"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("success", Matchers.equalTo(false)))
+                .andExpect(model().attribute("exception", Matchers.equalTo("java.lang.RuntimeException: was called")))
+                .andExpect(model().attribute("post", Matchers.equalTo(true)));
+    }
+
+    @Test
+    public void checkCallDeleteFile() throws Exception {
+        given(restTemplate.exchange(Application.getSettings().getBackendUrl()+"/ccms/delete/thumbnail", HttpMethod.DELETE, testUtil.createTestHttpEntityNoBody(), String.class))
+                .willThrow(new RuntimeException("was called"));
+
+        this.mockMvc.perform(post("/video/remove/video1")
+                .param("thumbnailFilename", "thumbnail")
+                .param("m3u8Filename", "m3u8")
+                .param("tsFilesName", "sequence.%d.ts")
+                .param("tsFilesCount", "2"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("success", Matchers.equalTo(false)))
+                .andExpect(model().attribute("exception", Matchers.equalTo("java.lang.RuntimeException: was called")))
+                .andExpect(model().attribute("post", Matchers.equalTo(true)));
+    }
 }
